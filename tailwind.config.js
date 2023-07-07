3381ec0f131e9539c397ecf7f113b9989c31988e
Javier Jaramillo <jjaramillo34@gmail.com>
2023-07-07 10:06:42 -0400

    Fix issue with font in config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,21 +14,20 @@ export const theme = {
     ...defaultTheme.screens,
   },
   darkMode: false,
-  theme: {
-    fontFamily: {
-      // 'sans': ['ui-sans-serif', 'system-ui', ...],
-      // 'serif': ['ui-serif', 'Georgia', ...],
-      // 'mono': ['ui-monospace', 'SFMono-Regular', ...],
-      // 'display': ['Oswald', ...],
-      // 'body': ['Open Sans', ...],
-      'league-gothic': 'League Gothic',
-      'league-gothic-regular': 'League Gothic-Regular',
-      'league-gothic-condensed-regular': 'League Gothic-Condensed Regular',
-      'league-gothic-italic': 'League Gothic-Italic',
-      'league-gothic-condensed-italic': 'League Gothic-Condensed Italic',
-      'rockwell': 'Rockwell',
-      'rockwell-bold': 'RockwellBold',
-      'helvetica': 'Helvetica'
-    }
-  },
+  
+  fontFamily: {
+    // 'sans': ['ui-sans-serif', 'system-ui', ...],
+    // 'serif': ['ui-serif', 'Georgia', ...],
+    // 'mono': ['ui-monospace', 'SFMono-Regular', ...],
+    // 'display': ['Oswald', ...],
+    // 'body': ['Open Sans', ...],
+    'league-gothic': 'League Gothic',
+    'league-gothic-regular': 'League Gothic-Regular',
+    'league-gothic-condensed-regular': 'League Gothic-Condensed Regular',
+    'league-gothic-italic': 'League Gothic-Italic',
+    'league-gothic-condensed-italic': 'League Gothic-Condensed Italic',
+    'rockwell': 'Rockwell',
+    'rockwell-bold': 'RockwellBold',
+    'helvetica': 'Helvetica'
+  }
 };
